b386dc09c9e796aed0f3ecf35f6d615e34e701c3
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-06-14 13:12:47 +0100

    07_booleans

---
+++ 07_booleans/code.py
@@ -0,0 +1,8 @@
+# comparisons: ==, != > ,< , >=, <=
+print(5 == 5)
+
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friends == abroad) # True = compares the values inside
+print(friends is abroad) # False = compares if the two elements are exact same thing, this case there are two different lists in memory containing the same values
