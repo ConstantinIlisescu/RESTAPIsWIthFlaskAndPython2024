c16efb82484e5d7a9dc3a23b7f47c60ce6a201f4
Constantin Ilisescu <ilisescu@amazon.com>
2024-09-19 15:26:49 +0100

    31_errors_in_python

---
+++ 31_errors_in_python/code.py
@@ -0,0 +1,19 @@
+def divide(divident, divisor):
+    if divisor ==0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+    
+    return divident / divisor
+
+
+grades = []
+
+print("Welcome to the average grade program.")
+try:
+    average = divide(sum(grades), len(grades))
+except ZeroDivisionError as e:
+    print("There are no grades yet in your list.")
+else:
+    ##this will only run if there is no except
+    print(f"The average grade is {average}.")
+finally:
+    print("Thank you")
